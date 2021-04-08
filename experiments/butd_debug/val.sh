ae3e95629a38e65d25a76e9784e9f1b64bf88ac2
RitaRamos <rita.mparada.ramos@gmail.com>
2021-04-08 15:56:28 +0100

    add other experiments files

--- experiments/butd_debug/val.sh
+++ experiments/butd_debug/val.sh
@@ -1,36 +1,34 @@
 IMAGE_NAME="image_features.hdf5"
 MODEL_NAME="BOTTOM_UP_TOP_DOWN"
 MODEL_ABBR="butd"
-MODEL_SUFF=""
+MODEL_SUFF="_debug"
 
 DATA_DIR="data/coco2014"
 IMGS_DIR="$DATA_DIR/images"
+EXP_DIR="$experiments/${MODEL_ABBR}${MODEL_SUFF}" #VER ESTE!!!
 CAPS_DIR="$DATA_DIR/datasets"
-CKPT_DIR="experiments/checkpoints/"
 LOG_DIR="myfirstlog"
+CKPT="experiments/${MODEL_ABBR}${MODEL_SUFF}/checkpoints/checkpoint.best.pth.tar"
 
-mkdir -p $CKPT_DIR $LOG_DIR
 
-train_args="""
+args="""
 	--image-features-filename $IMGS_DIR/$IMAGE_NAME \
   --dataset-splits-dir $CAPS_DIR \
-  --checkpoints-dir $CKPT_DIR \
+  --checkpoint $CKPT \
 	--logging-dir $LOG_DIR \
-	--objective GENERATION \
-  --batch-size 2 \
-	--max-epochs 2 \
-	--epochs-early-stopping 5 \
+	--output-path $EXP_DIR \
+	--split val \
 	--max-caption-len 20 \
-	--print-freq 10 \
-	--debug \
-"""
-
-model_args="""
-butd
+	--beam-size 100 \
+	--eval-beam-size 5 \
 """
 
 #source activate /envs/syncap
 
 export PYTHONWARNINGS="ignore"
 
-time python3 src/train.py $train_args $model_args
+time python3 src/eval.py $args
+
+#conda deactivate
+
+
