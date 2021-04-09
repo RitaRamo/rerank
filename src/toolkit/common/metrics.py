de16c0b72f4010c1b578a09de327ab81f2107399
RitaRamos <rita.mparada.ramos@gmail.com>
2021-04-09 11:50:08 +0100

    add key for annotations

--- src/toolkit/common/metrics.py
+++ src/toolkit/common/metrics.py
@@ -40,6 +40,14 @@ def coco_metrics(generated_captions_fn, annotations_dir, split):
     print("ann_fn", ann_fn)
     ann_fn['type'] = 'captions'
 
+    with open(ann_fn, 'r') as f:
+        data = json.load(f)
+        data['type'] = 'captions'
+    with open(annotations_dir+'/annotations/captions_val2014_new.json', 'w') as f:
+        json.dump(data, f)
+
+    ann_fn = annotations_dir+'/annotations/captions_val2014_new.json'
+    print("new ann_fn", ann_fn)
     coco = COCO(ann_fn)
     cocoRes = coco.loadRes(generated_captions_fn)
 
