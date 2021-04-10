849715f314731cdc549b24f9410082a6da26ab22
RitaRamos <rita.mparada.ramos@gmail.com>
2021-04-10 12:20:38 +0100

    try again coco caption

--- src/coco_caption/pycocoevalcap/spice/spice.py
+++ src/coco_caption/pycocoevalcap/spice/spice.py
@@ -51,7 +51,7 @@ class Spice:
         if not os.path.exists(temp_dir):
           os.makedirs(temp_dir)
         in_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir)
-        in_file.write(json.dumps(input_data, indent=2).encode('utf-8'))
+        json.dump(input_data, in_file, indent=2)
         in_file.close()
 
         # Start job
@@ -61,7 +61,7 @@ class Spice:
         if not os.path.exists(cache_dir):
           os.makedirs(cache_dir)
         spice_cmd = ['java', '-jar', '-Xmx8G', SPICE_JAR, in_file.name,
-          #'-cache', cache_dir,
+          '-cache', cache_dir,
           '-out', out_file.name,
           '-subset',
           '-silent'
@@ -85,7 +85,7 @@ class Spice:
         for image_id in imgIds:
           # Convert none to NaN before saving scores over subcategories
           score_set = {}
-          for category,score_tuple in imgId_to_scores[image_id].items():
+          for category,score_tuple in imgId_to_scores[image_id].iteritems():
             score_set[category] = {k: self.float_convert(v) for k, v in score_tuple.items()}
           scores.append(score_set)
         return average_score, scores
