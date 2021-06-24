2651de394ce94b9e61f67567b81a2c56569df0a4
RitaRamos <rita.mparada.ramos@gmail.com>
2021-06-24 20:26:05 +0100

    new context retrieval

--- src/toolkit/data/datasets.py
+++ src/toolkit/data/datasets.py
@@ -465,7 +465,7 @@ def get_data_loader(split, batch_size, dataset_splits_dir, image_features_fn, wo
     elif split == "context_retrieval":
         data_loader = torch.utils.data.DataLoader(
                 CaptionTrainContextRetrievalDataset(dataset_splits_dir, image_features_fn, normalize, features_scale_factor),
-                batch_size=1000, shuffle=True, num_workers=0, pin_memory=False
+                batch_size=10, shuffle=True, num_workers=0, pin_memory=False
             )
 
     else:
