8626b116b20f667cce5773eec1fa6fc6619d30fc
RitaRamos <rita.mparada.ramos@gmail.com>
2021-06-24 19:46:23 +0100

    new context retrieval

--- src/toolkit/data/datasets.py
+++ src/toolkit/data/datasets.py
@@ -231,19 +231,34 @@ class ContextRetrieval():
     def train_retrieval(self, train_dataloader_images):
         print("starting training")
         start_training=True
+        all_images_and_text_context=torch.tensor([])
+        all_targets=torch.tensor([])
+        is_to_add = False
+
         for (images, contexts, targets) in tqdm(train_dataloader_images):
             #add to the datastore
             #print("context added", targets)
             enc_contexts=self.sentence_model.encode(contexts)
             images_and_text_context = numpy.concatenate((images.mean(dim=1).numpy(),enc_contexts), axis=-1) #(n_contexts, 2048 + 768)
-          
+            all_images_and_text_context = numpy.concatenate((all_images_and_text_context,images_and_text_context)) 
+            print("all all_images_and_text_context", all_images_and_text_context.size)
+            all_targets=numpy.concatenate((all_targets,targets))
+            
             #self.datastore.add(images_and_text_context)
-            if start_training:
-                print("training")
-                self.datastore.train(images_and_text_context)
-                self.datastore.add_with_ids(images_and_text_context, numpy.array(targets))
-                start_training = False
-            else:
+            if len(all_targets)>2000000:
+                if start_training:
+                    print("training")
+                    self.datastore.train(images_and_text_context)
+                    self.datastore.add_with_ids(images_and_text_context, numpy.array(targets))
+                    start_training = False
+                    is_to_add=True
+                    all_images_and_text_context=torch.tensor([])
+                    all_targets=torch.tensor([])
+                else:
+                    all_images_and_text_context=torch.tensor([])
+                    all_targets=torch.tensor([])
+
+            if is_to_add: #only start adding after training (reaching 2.000.000 vectors)
                 print("adding")
                 #targets = torch.tensor(targets).to(self.device)
                 #self.targets_of_dataloader= torch.cat((self.targets_of_dataloader,targets))
@@ -450,7 +465,7 @@ def get_data_loader(split, batch_size, dataset_splits_dir, image_features_fn, wo
     elif split == "context_retrieval":
         data_loader = torch.utils.data.DataLoader(
                 CaptionTrainContextRetrievalDataset(dataset_splits_dir, image_features_fn, normalize, features_scale_factor),
-                batch_size=2000000, shuffle=True, num_workers=0, pin_memory=False
+                batch_size=10000, shuffle=True, num_workers=0, pin_memory=False
             )
 
     else:
