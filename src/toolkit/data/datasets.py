697434d494bbe53fb6c3e886e6e7321cb1e3b916
RitaRamos <rita.mparada.ramos@gmail.com>
2021-07-03 17:18:43 +0100

    score val

--- src/toolkit/data/datasets.py
+++ src/toolkit/data/datasets.py
@@ -254,7 +254,7 @@ class ContextRetrieval():
                 print("added_so_far:(added_so_far+batch_size)", (added_so_far+batch_size))
                 print("alll images", len(all_images_and_text_context[added_so_far:(added_so_far+batch_size),:]))
                 all_images_and_text_context[added_so_far:(added_so_far+batch_size),:] = numpy.concatenate((images.mean(dim=1).numpy(),self.sentence_model.encode(contexts)), axis=-1)    
-                all_targets[added_so_far:(added_so_far+batch_size),:]=targets
+                all_targets[added_so_far:(added_so_far+batch_size)]=targets
                 added_so_far=+batch_size
 
                 if added_so_far>=max_to_fit_in_memory:
