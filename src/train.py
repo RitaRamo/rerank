32754dd408d55f63266306f07b8a4550bc61dd76
RitaRamos <rita.mparada.ramos@gmail.com>
2021-08-06 10:21:58 +0100

    debug

--- src/train.py
+++ src/train.py
@@ -420,11 +420,9 @@ def main(args):
                                         0, args.image_normalize)
  
         
-        model_args="""
-        butd
-        """
-
-        context_args=check_model_args(model_args)
+        context_model_args="butd"
+        print("entrei")
+        context_args=check_model_args(context_model_args)
         context_model = BUTDModel(context_args)
         print("self con", context_model)
         
