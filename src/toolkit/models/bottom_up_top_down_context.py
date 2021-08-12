34be59a7a0d5b9f5f5c554476f25593864f285cf
RitaRamos <rita.mparada.ramos@gmail.com>
2021-08-12 15:54:05 +0100

    debug

--- src/toolkit/models/bottom_up_top_down_context.py
+++ src/toolkit/models/bottom_up_top_down_context.py
@@ -124,7 +124,7 @@ class TopDownDecoder(CaptioningDecoder):
 
         return scores, states, None
 
-    def interpolate(self, scores, encoder_output, prev_words, retrieval, target_lookup, interpolation=0., k_neighbours=16):
+    def interpolate(self, scores, encoder_output, prev_words, retrieval, target_lookup, interpolation=0., k_neighbours=64):
         #print("socres", scores)
         softmax_scores = self.softmax(scores)
         #print("socres log softmax", softmax_scores)
